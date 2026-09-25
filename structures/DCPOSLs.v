46d34f4863cd148fd08752d58e4ca5b441c36e17
arthurovale <34468091+arthurovale@users.noreply.github.com>
2026-09-24 20:50:43 -0400

    egli-milner DCPO structure

--- structures/DCPOSLs.v
+++ structures/DCPOSLs.v
@@ -13,6 +13,9 @@ Require Import Stdlib.Logic.Classical.
 Require Import Stdlib.Logic.ClassicalChoice.
 Require Import Stdlib.Logic.ChoiceFacts.
 
+Require Import Stdlib.Lists.List.
+Require Import Stdlib.Arith.PeanoNat.
+
 
 (** * Dcpo-lattices *)
 
@@ -174,6 +177,69 @@ Class IsFDCPOSL (P F : Type) `{Pdcpo : DCPO P} `{Fdcposl : DCPOSL F} :=
       (forall p, g (emb p) = f p) -> forall x, g x = ext f x;
   }.
 
+Class Finitary `{PartialOrder} := 
+  finitary : forall d : P, exists l : list P, forall a, R a d <-> In a l.  
+
+Lemma directed_bounded_in `{DCPO} {I} `{inhabited I} (x : I -> P) `{Dx : !Directed x} (l : list P) :
+  exists i, forall j, In (x j) l -> lce (x j) (x i).
+Proof.
+  destruct inhabited0 as [i0].
+  induction l as [| a l IH].
+  - exists i0. intros j [].
+  - destruct IH as [i Hi].
+    destruct (classic (exists j0, x j0 = a)) as [[j0 Hj0] | Hno].
+    + destruct (Dx j0 i) as [k [Hj0k Hik]].
+      exists k. intros j [Ha | Hl].
+      * rewrite <- Ha, <- Hj0. exact Hj0k.
+      * transitivity (x i); auto.
+    + exists i. intros j [Ha | Hl].
+      * destruct Hno. exists j. auto.
+      * auto.
+Qed.
+
+Lemma finitary_DCPO_acc `{DCPO} `{!Finitary (R := lce)} {I} `{inhabited I} (x : I -> P) `{!Directed x} :
+  exists i, forall j, lce (x j) (x i).
+Proof.
+  destruct (finitary (dsup x)) as [below Hbelow].
+  destruct (directed_bounded_in (inhabited0 := inhabited0)  x below) as [i Hi].
+  exists i. intros j. apply Hi, Hbelow, dsup_ub.
+Qed.
+
+Lemma monotone_seq_directed `{DCPO} (g : nat -> P) :
+  (forall n, lce (g n) (g (S n))) -> Directed g.
+Proof.
+  intros Hg.
+  assert (Hmon : forall n m, (n <= m)%nat -> lce (g n) (g m)).
+  { intros n m Hnm. induction Hnm. reflexivity. transitivity (g m); auto. }
+  intros n m. exists (Nat.max n m).
+  split; apply Hmon. apply Nat.le_max_l. apply Nat.le_max_r.
+Qed.
+
+Lemma finitary_DCPO_maximal `{DCPO} `{!Finitary (R := lce)} (A : P -> Prop) :
+  (exists b, A b) ->
+  exists b, A b /\ forall b', A b' -> lce b b' -> b = b'.
+Proof.
+  intros [b0 Hb0].
+  destruct (classic (exists b, A b /\ forall b', A b' -> lce b b' -> b = b'))
+    as [Hmax | Hno]; [assumption | exfalso].
+  (* every element of A has a strict successor in A *)
+  assert (Hstep : forall b, exists b', A b -> A b' /\ lce b b' /\ b <> b').
+  { intros b. destruct (classic (A b)) as [Hb | Hb].
+    - apply NNPP. intros Hn. apply Hno. exists b. split; auto.
+      intros b' Hb' Hle. apply NNPP. intros Hneq. apply Hn. exists b'. auto.
+    - exists b. tauto. }
+  destruct (choice _ Hstep) as [f Hf].
+  pose (g := nat_rect (fun _ => P) b0 (fun _ b => f b)).
+  assert (HgS : forall n, A (g n)).
+  { induction n; cbn. assumption. apply Hf; assumption. }
+  assert (Hg : forall n, lce (g n) (g (S n)) /\ g n <> g (S n)).
+  { intros n. apply Hf, HgS. }
+  pose proof (monotone_seq_directed g (fun n => proj1 (Hg n))) as Hdir.
+  destruct (finitary_DCPO_acc (inhabited0 := inhabits 0) g) as [i Hi].
+  destruct (Hg i) as [Hle Hneq].
+  apply Hneq, antisymmetry; auto.
+Qed.
+
 Section EGLI_MILNER_DOMAIN.
   Context `{DCPO}.    
 
@@ -286,10 +352,14 @@ Section EGLI_MILNER_DOMAIN.
 
   Record em_le (x y : convex_set) :=
     {
+      (** fw: no information is dropped, if a in x then it evolves to some b in y *)
+      (** different from standard EM, we consider bot and do not enforce fw for it *)
       em_le_fw : forall (a : P), x a -> a <> bot -> exists (b : P) , y b /\ lce a b;
+      (** anything in y needs to be justified by something in x *)
       em_le_bw : forall (b : P) , y b -> exists (a : P) , x a /\ lce a b
     }.
 
+  (** bot can only be removed forwardly *)
   Lemma le_div (x y : convex_set) :
     em_le x y -> y bot -> x bot.
   Proof.
@@ -298,6 +368,7 @@ Section EGLI_MILNER_DOMAIN.
     pose lce_bot_eq as Heq. specialize (Heq a lcea). rewrite <- Heq. exact in_x.
   Qed.
 
+  (* the bottom element for em_le is {div} *)
   Program Definition div_set := 
   {|
     mem x := x = bot;
@@ -335,7 +406,7 @@ Section EGLI_MILNER_DOMAIN.
   Qed.
 
   Lemma em_le_incl (x y : convex_set) :
-    em_le x y -> em_le y x -> forall a, x a -> y a.
+    em_le x y -> em_le y x -> incl x y.
   Proof.
     intros Hxy Hyx a Hxa.
     destruct (classic (a = bot)) as [-> | Ha].
@@ -359,3 +430,164 @@ Section EGLI_MILNER_DOMAIN.
   Proof.
     split; typeclasses eauto.
   Qed.
+
+  (** the supremum is the set of all elements that are eventually always present *)
+  (** bot is only present if it never gets removed *)
+  Definition dsup_em_mem {I} (x : I -> convex_set) `{Hx: !Directed x} : P -> Prop :=
+    fun a => (a = bot -> forall i, x i bot) /\
+             (a <> bot -> exists i, forall j, em_le (x i) (x j) -> x j a).
+
+  (** Convexity follows straight-forwardly *)
+  Lemma dsup_em_convexity {I} (x : I -> convex_set) `{Hx: !Directed x} : 
+    forall a b c, dsup_em_mem x a -> dsup_em_mem x c -> lce a b -> lce b c -> dsup_em_mem x b.
+  Proof.
+    intros a b c [Ha_bot Ha] [Hc_bot Hc] lce_a_b lce_b_c.
+    split. 
+    - intros ->. apply Ha_bot. apply lce_bot_eq. assumption.
+    - intros b_neq_bot.
+      assert (c_neq_bot : c <> bot).
+      { intros ->. apply b_neq_bot, lce_bot_eq. assumption. }
+      destruct (Hc c_neq_bot) as [j Hj].
+      destruct (classic (a = bot)) as [-> | a_neq_bot].
+      + (* bot is in every x i *)
+        exists j. intros k em_le_j_k.
+        apply (convexity (x k) bot b c); auto.
+      + destruct (Ha a_neq_bot) as [i Hi].
+        destruct (Hx i j) as [k [em_le_i_k em_le_j_k]].
+        exists k. intros k' em_le_k_k'.
+        apply (convexity (x k') a b c); auto.
+        apply Hi. transitivity (x k); assumption.
+        apply Hj. transitivity (x k); assumption.
+  Qed.
+
+  Definition dsup_em {I} (x : I -> convex_set) `{Hx: !Directed x} : convex_set := 
+  {|
+    mem := dsup_em_mem x;
+    convexity := dsup_em_convexity x;
+  |}.
+
+  (** If a finite set of elements are all eventually always absent, then there is 
+  a stage when they are all simultaneously absent. *)
+  Lemma eventually_absent_all {I} (x : I -> convex_set) `{Hx : !Directed x} `{inhabited I} (b : P) (l : list P) :
+    (forall a, In a l -> lce a b -> exists i, forall j, em_le (x i) (x j) -> ~ x j a) ->
+    exists i, forall j, em_le (x i) (x j) -> forall a, In a l -> lce a b -> ~ x j a.
+  Proof.
+    destruct inhabited0 as [i0].
+    induction l as [| a l IH]; intros Habs.
+    - exists i0. intros j _ a [].
+    - destruct (IH (fun a' Hin Hab => Habs a' (or_intror Hin) Hab)) as [i1 Hi1].
+      destruct (classic (lce a b)) as [Hab | Hab].
+      + destruct (Habs a (or_introl eq_refl) Hab) as [ia Hia].
+        destruct (Hx i1 ia) as [k [Hk1 Hka]].
+        exists k. intros j Hkj a' [<- | Hin] Ha'b.
+        * apply Hia. transitivity (x k); assumption.
+        * exact (Hi1 j (transitivity Hk1 Hkj) a' Hin Ha'b).
+      + exists i1. intros j Hj a' [<- | Hin] Ha'b.
+        * contradiction.
+        * exact (Hi1 j Hj a' Hin Ha'b).
+  Qed.
+
+  Global Program Instance em_dcpo `{!Finitary (R := lce)} : DCPO convex_set := 
+  {|
+    lce := em_le;
+    dsup := @dsup_em;
+  |}.
+  Next Obligation.
+    split.
+    (* upper bound *)
+    - split.
+      (* fw *)
+      intros a in_x_i_a neq_bot.
+      (* we define a set S of all elements in stages after i such that a <= b *)
+      pose (S := fun b => (exists j, em_le (x i) (x j) /\ x j b /\ lce a b)).
+      (* S is non-empty as a is in S *)
+      assert (inS: S a).
+      { exists i. split. reflexivity. split. assumption. reflexivity. }
+      (* since the DCPO is finitary this set has a maximal element *)
+      assert (Smax: exists b, S b /\ forall b', S b' -> lce b b' -> b = b').
+      { apply finitary_DCPO_maximal. exists a. apply inS. }
+      destruct Smax as [b [binS Hbmax]].
+      destruct binS as [ib [em_le_i_ib [in_x_ib lce_a_b]]].
+      exists b. split.
+      (* since b is above a which is assumed not bot b must not be bot *)
+      assert (b_neq_bot: b <> bot).
+      { intros Heq. apply neq_bot. apply lce_bot_eq. subst; assumption. }
+      (* The key part of the proof is now showing that for any stage after ib
+         b will be ther. This can be shown by the forward condition em_le (x ib) (x j)
+         since any b' given by it satisfies the definition of S and by maximality
+         must be equal to b.
+      *)
+      split; [intros; contradiction | intros _].
+      exists ib. intros j em_le_ib_j.
+      destruct (em_le_fw _ _ em_le_ib_j b in_x_ib b_neq_bot) as [b' [in_x_j_b' lce_b_b']].
+      assert (b = b') as <-.
+      { apply Hbmax. 
+        exists j. split. transitivity (x ib); assumption. split.
+        assumption. transitivity b; assumption.
+        assumption.
+      }
+      assumption. 
+      (* By definition of S, a <= b *)
+      assumption.
+      (* bw *)
+      intros b [b_bot_dsup b_neq_bot_dsup].
+      destruct (classic (b = bot)) as [b_eq | b_eq].
+      * exists bot. split; [apply b_bot_dsup; assumption | apply bot_lb].
+      * destruct (b_neq_bot_dsup b_eq) as [j Hdsup].
+        destruct (directed i j) as [k [le_i_k le_j_k]].
+        destruct le_i_k as [_ le_i_k_bw].
+        apply le_i_k_bw. apply Hdsup. assumption.
+  (* least *)
+  - intros y y_ub.
+    split.
+    (* fw *)
+    + intros a [_ in_dsup_em] a_neq_bot.
+      destruct (in_dsup_em a_neq_bot) as [i Hdsup].
+      specialize (y_ub i).
+      destruct y_ub as [y_ub_fw _].
+      (* the forward case follows straight-forwardly by using fw in the assumption *)
+      apply (y_ub_fw a).
+      apply Hdsup. reflexivity. assumption.
+    (* bw *)
+    (* the backward direction is the meat of the proof *)
+    + intros b in_y.
+      (* first we handle the bot case, as we then know bot is in all
+      stages since bot propagates backward from y *)
+      destruct (classic (b = bot)) as [b_eq | b_eq].
+      * exists bot. split.
+        split. 
+          intros. eapply le_div. apply y_ub. subst. assumption.
+          contradiction.
+        apply bot_lb.
+      (* if b is not bot then first we consider the empty directed sup separately *)
+      * destruct (classic (inhabited I)) as [Hinh | Hempty].
+        (* I not empty *)
+        (* First, we find the list of elements ↓b *)
+        destruct (finitary b) as [l Hl].
+        (* From the backward direction we can show that there is an a that is always eventually present *)
+        (* Here we prove by contradiction since otherwise it is always eventually absent. *)
+        assert (Hcof : exists a, lce a b /\ forall i, exists j, em_le (x i) (x j) /\ x j a).
+        { apply NNPP. intros Hno.
+          destruct (eventually_absent_all (inhabited0 := Hinh) x b l) as [i Hi].
+          { intros a Hin Hab. apply NNPP. intros Hn. apply Hno. exists a. split; auto.
+            intros i. apply NNPP. intros Hn'. apply Hn.
+            exists i. intros j Hij Hja. apply Hn'. exists j. auto. }
+          destruct (y_ub i) as [_ y_bw].
+          destruct (y_bw b in_y) as [a [Hia Hab]].
+          exact (Hi i (reflexivity _) a (proj1 (Hl a) Hab) Hab Hia). }
+        destruct Hcof as [a [Hab Hcof]].
+        (* This a is the a we need, as can be shown using convenxity *)
+        exists a. split; [split | assumption].
+        { intros -> i. destruct (Hcof i) as [j [Hij Hj]]. exact (le_div _ _ Hij Hj). }
+        intros Ha. destruct Hinh as [i0]. destruct (Hcof i0) as [i [_ Hi]].
+        exists i. intros j Hij.
+        destruct (Hcof j) as [k [Hjk Hk]].
+        destruct (em_le_fw _ _ Hij a Hi Ha) as [e [Hje Hae]].
+        destruct (em_le_bw _ _ Hjk a Hk) as [d [Hjd Hda]].
+        apply (convexity (x j) d a e); assumption.
+        (* If I is empty then the sup is just bot *)
+        exists bot. split; [split | apply bot_lb].
+        intros _ i. exfalso. apply Hempty. exact (inhabits i).
+        contradiction.
+  Qed.
+
